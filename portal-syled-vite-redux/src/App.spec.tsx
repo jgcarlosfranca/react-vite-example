f5e219ca5646f1e99a9c86eea43a885ad6a8fef1
joaogcarlosf <joaogcarlosf@gmail.com>
2023-05-04 15:49:54 -0300

    teste com fireEvent e run do coverage

--- portal-syled-vite-redux/src/App.spec.tsx
+++ portal-syled-vite-redux/src/App.spec.tsx
@@ -1,14 +1,29 @@
 import { expect, test } from "vitest"
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from './App'
 
 /**
  * Test default values by rendering a context consumer without a
  * matching provider
  */
-test('App shows default value', () => {
-    render(<App />)
-    expect(screen.getByText("Vite + React")).toHaveTextContent(
-        'Vite + React',
-    )
+
+describe("App tests", () => {
+    test('App shows default value', () => {
+        render(<App />)
+        expect(screen.getByText("Vite + React")).toHaveTextContent(
+            'Vite + React',
+        )
+    })
+
+
+    test('Should be able to render button count', () => {
+        render(<App />)
+        expect(screen.getByRole('button', { name: "count is 0" })).toBeInTheDocument()
+    })
+
+    test('Should be able to click on button count', async () => {
+        render(<App />)
+        fireEvent.click(screen.getByRole('button', { name: "count is 0" }))
+        expect(screen.getByRole('button', { name: "count is 1" })).toBeInTheDocument()
+    })
 })
