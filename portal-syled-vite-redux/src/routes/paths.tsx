20e660f1e97e9c90e5046941ef335ac2cb8d0880
joaogcarlosf <joaogcarlosf@gmail.com>
2023-05-15 22:21:05 -0300

    criação do path

---
+++ portal-syled-vite-redux/src/routes/paths.tsx
@@ -0,0 +1,15 @@
+const paths = {
+  Root: "/",
+  Calendar: "/Calendar",
+  Forms: "/Forms",
+  Modal: "Modal",
+  Reports: "/Reports",
+  Redux: "/Redux",
+  ReduxSimple: "/ReduxSimple",
+  Tools: "/Tools",
+  LabFont: "Lab/Fonts",
+  Beautify: "/Beautify",
+  Table: "Tables",
+};
+
+export { paths };
