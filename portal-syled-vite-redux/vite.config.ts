75451cf69fbddcf243dd4df127311826b604d845
joaogcarlosf <joaogcarlosf@gmail.com>
2023-06-21 21:10:21 -0300

    configurações de plugins

--- portal-syled-vite-redux/vite.config.ts
+++ portal-syled-vite-redux/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: { port: 3030 },
+
+  plugins: [svgr(), react(), reactRefresh()],
+  define: {
+    "process.env": process.env,
+  },
+});
