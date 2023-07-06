bcc0cb4cba27ed420ee72606057842d12c32b714
joaogcarlosf <joaogcarlosf@gmail.com>
2023-07-06 19:27:59 -0300

    fix provider do redux

--- portal-syled-vite-redux/src/main.tsx
+++ portal-syled-vite-redux/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { makeMirageServer } from "./Mocks/Server/server";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/Store";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./styles/reset.css";
+
+if (import.meta.env.DEV) {
+  makeMirageServer();
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
