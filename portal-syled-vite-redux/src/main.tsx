0a95eee10d7d4cadc209ea6eb772fb95c73e66c1
joaogcarlosf <joaogcarlosf@gmail.com>
2023-07-19 22:48:16 -0300

    fiz reset css

--- portal-syled-vite-redux/src/main.tsx
+++ portal-syled-vite-redux/src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/Store";
 import App from "./App";
 
-import "./styles/reset.css";
+import "./Styles/reset.css";
 
 if (import.meta.env.DEV) {
   makeMirageServer();
