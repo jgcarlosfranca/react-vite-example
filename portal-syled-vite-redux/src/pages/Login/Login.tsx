85f0654287072715bd8e59cace04b49f7ccd5d54
joaogcarlosf <joaogcarlosf@gmail.com>
2023-07-27 22:04:45 -0300

    criacao de uma tela de login basica

--- portal-syled-vite-redux/src/pages/Login/Login.tsx
+++ portal-syled-vite-redux/src/pages/Login/Login.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useState, ChangeEvent } from 'react';
+import { useLoginMutation } from '../../redux/Feature/loginSlice';
+import Loading from '../TransitionPages/Loading/Loading';
+
+interface IFormState {
+  user: string;
+  password: string;
+  canSeePassword: boolean;
+}
+
 
 const Login = () => {
+  const [loginApi, { isLoading, isError }] = useLoginMutation();
+  const [formFields, setFormFields] = useState<IFormState>({
+    user: "",
+    password: "",
+    canSeePassword: false,
+  });
+
+  const handleChangeInput = (
+    e: ChangeEvent<HTMLInputElement>,
+    setStateFn: React.Dispatch<React.SetStateAction<IFormState>>
+  ) => {
+    const type = e.target.type;
+    const name = e.target.name;
+    if (type === "checkbox") {
+      setStateFn((prevData) => ({ ...prevData, [name]: e.target.checked }));
+    } else {
+      setStateFn((prevData) => ({ ...prevData, [name]: e.target.value }));
+    }
+  };
+
+  const handleSubmit = async (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ): Promise<void> => {
+    event?.preventDefault();
+    try {
+      const autResponse = await loginApi({
+        email: formFields.user,
+        senha: formFields.password,
+      });
+      console.log('resposta', autResponse)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isLoading) {
+    return <Loading text='Carregando ...' textLength='2rem' />
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <input type='text' name='user' placeholder='Insira o usuario' value={formFields.user} onChange={(ev: ChangeEvent<HTMLInputElement>) =>
+        handleChangeInput(ev, setFormFields)
+      } />
+      <input type='password' name='password' placeholder='Insira a senha' value={formFields.password} onChange={(ev: ChangeEvent<HTMLInputElement>) =>
+        handleChangeInput(ev, setFormFields)
+      } />
+      <button onClick={
+        handleSubmit as unknown as React.MouseEventHandler<HTMLButtonElement>
+      }> Submit </button>
+
+    </div>
   )
 }
 
-export default Login
+export default Login;
