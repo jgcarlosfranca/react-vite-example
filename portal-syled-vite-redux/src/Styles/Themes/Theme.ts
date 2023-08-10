372dcb9069b4a08033fb5831491c49101529124c
joaogcarlosf <joaogcarlosf@gmail.com>
2023-08-09 23:00:14 -0300

    criacao de arquivo de thema

---
+++ portal-syled-vite-redux/src/Styles/Themes/Theme.ts
@@ -0,0 +1,29 @@
+//########## font https://pokepalettes.com/
+const Theme = {
+  color: {
+    primaria: {
+      vermelho_fire: "#de5239",
+    },
+    secundaria: {
+
+    },
+    apoio: {},
+    alerta: { sucesso: "#73ac31", erro: "#e63900", alerta: "#f6e652" },
+    neutra: {
+      neutra_background: "#F9F9F9",
+      cinza_linhas: "#CCCCCC",
+      cinza_texto: "#666666",
+      cinza_icones: "#DDDDDD",
+      cinza_fundo: "#F7F7F7",
+      cinza_field_background: "#EEEEEE",
+      preta: "#000",
+      branca: "#FFFFFF",
+      transparent: "#FFF0",
+      label_cinza: "#6a6a65",
+    },
+    opacity50: {
+    },
+  },
+};
+
+export default Theme;
