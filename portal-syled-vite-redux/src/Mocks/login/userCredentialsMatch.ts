7c2d22e71bcab37afc0708337bb2f687155f3139
joaogcarlosf <joaogcarlosf@gmail.com>
2024-01-30 13:15:31 -0300

    criação de sistema mockado de login com credenciais

---
+++ portal-syled-vite-redux/src/Mocks/login/userCredentialsMatch.ts
@@ -0,0 +1,20 @@
+export const userCredentialMatches = [
+    {
+      user: {
+        email: "admin@email.com",
+        password: "123",
+      },
+    },
+    {
+      user: {
+        email: "user1@email.com",
+        password: "654",
+      },
+    },
+    {
+      user: {
+        email: "user2@email.com",
+        password: "789",
+      },
+    },
+  ];
