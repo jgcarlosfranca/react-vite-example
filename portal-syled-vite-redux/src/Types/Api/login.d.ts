8a9bc6a05ba2f3ff9f5afae5b1d96b6dc24e6af5
joaogcarlosf <joaogcarlosf@gmail.com>
2024-01-31 22:25:03 -0300

    colocando novos args na interface de login response

--- portal-syled-vite-redux/src/Types/Api/login.d.ts
+++ portal-syled-vite-redux/src/Types/Api/login.d.ts
@@ -1,5 +1,10 @@
 export interface ILoginResponse {
+    cidade?: string;
+    email?: string;
+    nomeUser?: string;
+    perfil?: string;
     token: string;
+    uf?: string;
 }
 
 export interface ILoginBody {
