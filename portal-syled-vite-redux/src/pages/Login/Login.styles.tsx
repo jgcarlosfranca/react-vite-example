71a31107257873220b1c06a59f43febe2809a0c0
joaogcarlosf <joaogcarlosf@gmail.com>
2024-02-16 11:30:24 -0300

    new styles

--- portal-syled-vite-redux/src/pages/Login/Login.styles.tsx
+++ portal-syled-vite-redux/src/pages/Login/Login.styles.tsx
@@ -16,27 +16,42 @@ export const LoginBackground = styled.div`
 export const LoginBoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
-  justify-content: center;
-  width: 30%;
-  height:45%;
+  justify-content: flex-start;
+  min-width: 30%;
+  min-height:35%;
   background-color: #f0f8ff81;
   border-radius: 16px;
 `
 export const LoginBox = styled.div`
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
-  width: 80%;
-  height:45%;
+  min-width: 80%;
   gap: 2rem;
   input {
     border-radius: 16px;
-    height:2.5rem;
+    font-size: 1.225rem;
+    padding: 0 4px 0 16px;
+    background-color: transparent;
+    border-width: 1px;
   }
   button{
     border-radius: 16px;
-    height:2.5rem;
+    font-size: 24px;
     background-color: #2E4362;
     color: white;
+    margin-bottom: 24px;
   }
+`
+export const LoginTitle = styled.span`
+  display: flex;
+  font-size: 4rem;
+  font-weight: bold;
+  color: #2E4362;
+  margin: 75px 0 1.25rem 0;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 `
